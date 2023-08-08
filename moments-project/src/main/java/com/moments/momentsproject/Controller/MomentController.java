5dcb28ab5829295fed60679252677e4c00a48326
Bruno Alcantara <120646838+BrunoAlcantaraGit@users.noreply.github.com>
2023-08-07 23:02:45 -0300

    Update MomentController
    Ativação do tratamento da Exeption no endpoint

--- moments-project/src/main/java/com/moments/momentsproject/Controller/MomentController.java
+++ moments-project/src/main/java/com/moments/momentsproject/Controller/MomentController.java
@@ -18,14 +18,13 @@ public class MomentController {
 
     @PostMapping("/salvarMoment")
     public ResponseEntity<Moment> Salvar(@RequestBody Moment moment) throws Exception {
-        return new ResponseEntity<>(momentService.salvarMomento(moment), HttpStatus.CREATED);
-        /*try {
+
+        try {
             return new ResponseEntity<>(momentService.salvarMomento(moment), HttpStatus.CREATED);
 
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);}
 
-    }*/
     }
 }
